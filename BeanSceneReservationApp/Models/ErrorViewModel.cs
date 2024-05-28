17a40c6adcb3f23824b9f2c9cfd32da579f1e468
Jacob Cooper <JacobCooper2302@gmail.com>
2024-05-28 10:03:38 +1000

    Add project files.

---
+++ BeanSceneReservationApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeanSceneReservationApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
