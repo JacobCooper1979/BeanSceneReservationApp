75acd4c52decb72fe395722e4e2573fbeeb53054
Jacob Cooper <JacobCooper2302@gmail.com>
2024-05-30 12:00:49 +1000

    style has been added resturant tables has been reset and requires more work as its modle is not valid when creating

--- BeanSceneReservationApp/Models/RestaurantTable.cs
+++ BeanSceneReservationApp/Models/RestaurantTable.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BeanSceneReservationApp.Models
 {
     public partial class RestaurantTable
     {
+        [Key]
         public int TableId { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string TableName { get; set; } = null!;
+
+        [ForeignKey("Area")]
         public int AreaId { get; set; }
-        public string? TableStatus { get; set; }
-        public string? AreaName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TableStatus { get; set; } = null!;
+
         public virtual Area Area { get; set; } = null!;
         public virtual ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
     }
