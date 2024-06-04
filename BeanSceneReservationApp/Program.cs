5c82b2a9d120b291a92124f2952735bb87bc25c3
Jacob Cooper <JacobCooper2302@gmail.com>
2024-06-05 09:22:36 +1000

    images fixed

--- BeanSceneReservationApp/Program.cs
+++ BeanSceneReservationApp/Program.cs
@@ -34,6 +34,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
---
+++ SQLQuery3.sql
@@ -0,0 +1,31 @@
+﻿USE master;
+GO
+
+-- Drop the database if it exists
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'BeanSeanReservationSystemsDBTest3')
+    DROP DATABASE BeanSeanReservationSystemsDBTest3;
+GO
+
+-- Create the database
+CREATE DATABASE BeanSeanReservationSystemsDBTest3;
+GO
+
+-- Switch to the new database context
+USE BeanSeanReservationSystemsDBTest3;
+GO
+
+
+-- Create the Areas table
+CREATE TABLE Areas (
+    AreaID INT PRIMARY KEY IDENTITY(1,1),
+    AreaName VARCHAR(50) NOT NULL,
+    Capacity INT NOT NULL,
+    CONSTRAINT chk_AreaName CHECK (AreaName IN ('Main', 'Outdoors', 'Balcony'))
+);
+
+-- Insert areas into Areas table
+INSERT INTO Areas (AreaName, Capacity) VALUES
+('Main', 100),
+('Outdoors', 50),
+('Balcony', 30);
+
